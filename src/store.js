c0e9c44431f22fea5fa886aae3a6d21d5cdc8576
Alekseev Sergei <nec13@mail.ru>
2023-11-21 18:35:53 +0300

    feat: complete tasks

--- src/store.js
+++ src/store.js
@@ -2,78 +2,111 @@
  * Хранилище состояния приложения
  */
 class Store {
-  constructor(initState = {}) {
-    this.state = initState;
-    this.listeners = []; // Слушатели изменений состояния
-  }
+	constructor(initState = {}) {
+		this.state = initState;
+		this.listeners = []; // Слушатели изменений состояния
+	}
 
-  /**
-   * Подписка слушателя на изменения состояния
-   * @param listener {Function}
-   * @returns {Function} Функция отписки
-   */
-  subscribe(listener) {
-    this.listeners.push(listener);
-    // Возвращается функция для удаления добавленного слушателя
-    return () => {
-      this.listeners = this.listeners.filter(item => item !== listener);
-    }
-  }
+	/**
+	 * Подписка слушателя на изменения состояния
+	 * @param listener {Function}
+	 * @returns {Function} Функция отписки
+	 */
+	subscribe(listener) {
+		this.listeners.push(listener);
+		// Возвращается функция для удаления добавленного слушателя
+		return () => {
+			this.listeners = this.listeners.filter(item => item !== listener);
+		};
+	}
 
-  /**
-   * Выбор состояния
-   * @returns {Object}
-   */
-  getState() {
-    return this.state;
-  }
+	/**
+	 * Выбор состояния
+	 * @returns {Object}
+	 */
+	getState() {
+		return this.state;
+	}
 
-  /**
-   * Установка состояния
-   * @param newState {Object}
-   */
-  setState(newState) {
-    this.state = newState;
-    // Вызываем всех слушателей
-    for (const listener of this.listeners) listener();
-  }
+	/**
+	 * Установка состояния
+	 * @param newState {Object}
+	 */
+	setState(newState) {
+		this.state = newState;
+		// Вызываем всех слушателей
+		for (const listener of this.listeners) listener();
+	}
 
-  /**
-   * Добавление новой записи
-   */
-  addItem() {
-    this.setState({
-      ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
-    })
-  };
+	/**
+	 * Добавление новой записи
+	 */
+	addItem() {
+		const newCode = this.generateUniqueCode();
+		const newNote = { code: newCode, title: 'Новая запись' };
 
-  /**
-   * Удаление записи по коду
-   * @param code
-   */
-  deleteItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.filter(item => item.code !== code)
-    })
-  };
+		this.setState({
+			...this.state,
+			list: this.state.list ? [...this.state.list, newNote] : [newNote]
+		});
+	}
 
-  /**
-   * Выделение записи по коду
-   * @param code
-   */
-  selectItem(code) {
-    this.setState({
-      ...this.state,
-      list: this.state.list.map(item => {
-        if (item.code === code) {
-          item.selected = !item.selected;
-        }
-        return item;
-      })
-    })
-  }
+	/**
+	 * Удаление записи по коду
+	 * @param code
+	 */
+	deleteItem(code) {
+		const deletedCodes = this.state.deletedCodes || [];
+
+		this.setState({
+			...this.state,
+			list: this.state.list.filter(item => item.code !== code),
+			deletedCodes: [...deletedCodes, code] // Добавляем код в список удаленных
+		});
+	}
+
+	/**
+	 * Выделение записи по коду
+	 * @param code
+	 */
+	selectItem(code) {
+		const updatedList = this.state.list.map(item => {
+			if (item.code === code) {
+				if (!item.selected) {
+					item.selectionCount = (item.selectionCount || 0) + 1; // Увеличиваем счетчик выделений
+				}
+				item.selected = !item.selected;
+			} else {
+				item.selected = false; // Сбрасываем выделение у других записей
+			}
+			return item;
+		});
+
+		this.setState({
+			...this.state,
+			list: updatedList
+		});
+	}
+
+	/**
+	 * Генерация уникального кода, учитывая удаленные записи
+	 * @returns {number}
+	 */
+	generateUniqueCode() {
+		const existingCodes = this.state.list?.map(item => item.code) || [];
+		const deletedCodes = this.state.deletedCodes || [];
+
+		// Объединяем существующие и удаленные коды
+		const allCodes = [...existingCodes, ...deletedCodes];
+
+		let newCode = 1;
+
+		while (allCodes.includes(newCode)) {
+			newCode++;
+		}
+
+		return newCode;
+	}
 }
 
 export default Store;
